cbd03b916ac7227639cb5f7fad34e51883b9bb56
Fixius50 <robertomonederoalonso0@gmail.com>
2024-08-24 00:28:53 +0200

    t

--- default/code/java/src/com/worldbuilding/Main.java
+++ default/code/java/src/com/worldbuilding/Main.java
@@ -16,13 +16,17 @@ public class Main extends Application {
 
         WebView webView = new WebView();
         File htmlFileMenuInicialLog = new File("default\\code\\html\\menuInicialLog.html");
+        
         if (htmlFileMenuInicialLog.exists()) {
             webView.getEngine().load(htmlFileMenuInicialLog.toURI().toString());
         } else {
             System.out.println("Error: El archivo HTML no se encuentra en la ruta especificada.");
         }
 
-        Scene scene = new Scene(root, 800, 600);
+        int ancho = (int) primaryStage.getMaxWidth();
+        int alto = (int) primaryStage.getMaxHeight();
+
+        Scene scene = new Scene(root, ancho, alto);
         // scene.getStylesheets().add("file:default/code/css/style.css");
         /*
          * La línea de arriba es para cuando se quieren enlazar el html al css y
@@ -32,8 +36,8 @@ public class Main extends Application {
         primaryStage.setTitle("Aplicación WorldBuilding");
         primaryStage.setScene(scene);
         
-        // Configurar la ventana para que se ponga en pantalla completa
-        primaryStage.setFullScreen(true);
+        // Configurar la ventana para que se ponga en pantalla completa;
+        primaryStage.setFullScreen(false);
 
         // Alternativamente, si deseas mantener los bordes del sistema operativo:
         // primaryStage.setMaximized(true);
