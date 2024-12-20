2ac8a50c85a3e83e0a9ab0554c793a2f1a4327e7
Fixius50 <robertomonederoalonso0@gmail.com>
2024-12-20 22:20:04 +0100

    a

--- default/code/java/src/com/worldbuilding/Main.java
+++ default/code/java/src/com/worldbuilding/Main.java
@@ -61,7 +61,7 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Función donde se inicia la app
         launch(args);
     }
 }
