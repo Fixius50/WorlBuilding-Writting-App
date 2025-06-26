ef8ac1d23b33f6dc7beb5a51096e79be46b4a4fe
Fixius50 <robertomonederoalonso0@gmail.com>
2025-06-26 15:57:36 +0200

    nuevos cambios

---
+++ src/main/java/com/worldbuilding/WorldbuildingApp/WorldbuildingAppApplication.java
@@ -0,0 +1,13 @@
+package com.worldbuilding.WorldbuildingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldbuildingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldbuildingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worldbuilding/WorldbuildingApp/WorldbuildingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldbuilding.WorldbuildingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldbuildingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
