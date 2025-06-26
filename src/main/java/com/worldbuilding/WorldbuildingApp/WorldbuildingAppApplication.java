9576992dc0193d999a34f2e2b983f799ef44c4db
Fixius50 <robertomonederoalonso0@gmail.com>
2025-06-26 23:38:35 +0200

    asd

--- src/main/java/com/worldbuilding/WorldbuildingApp/WorldbuildingAppApplication.java
+++ src/main/java/com/worldbuilding/WorldbuildingApp/WorldbuildingAppApplication.java
@@ -2,6 +2,8 @@ package com.worldbuilding.WorldbuildingApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.awt.Desktop;
+import java.net.URI;
 
 @SpringBootApplication
 public class WorldbuildingAppApplication {
@@ -9,7 +11,13 @@ public class WorldbuildingAppApplication {
 	 * Primero arrancar XAMPP e iniciar MySQL
 	 * Luego: ejecutar la app y poner en el navegador http://localhost:8080 y de seguido la ruta relativa.
 	 */
-	public static void main(String[] args) {
-		SpringApplication.run(WorldbuildingAppApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldbuildingAppApplication.class, args);
+        try {
+            Desktop.getDesktop().browse(new URI("http://localhost:8080/menuInicialLog.html"));
+        } catch (Exception e) {
+            System.err.println("No se pudo abrir el navegador automáticamente: " + e.getMessage());
+        }
+    }
 }
