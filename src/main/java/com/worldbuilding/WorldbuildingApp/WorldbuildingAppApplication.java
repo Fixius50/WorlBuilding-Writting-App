f5c8e497c4592c45864ec95a40a5578865a01cf5
Fixius50 <robertomonederoalonso0@gmail.com>
2025-06-26 23:26:56 +0200

    sfd

--- src/main/java/com/worldbuilding/WorldbuildingApp/WorldbuildingAppApplication.java
+++ src/main/java/com/worldbuilding/WorldbuildingApp/WorldbuildingAppApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorldbuildingAppApplication {
-
+	/**
+	 * Primero arrancar XAMPP e iniciar MySQL
+	 * Luego: ejecutar la app y poner en el navegador http://localhost:8080 y de seguido la ruta relativa.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(WorldbuildingAppApplication.class, args);
 	}
-
 }
