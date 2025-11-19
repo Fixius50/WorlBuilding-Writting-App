02f9efe1d83cd04ffc6f5fab0549e6321156b8e3
Fixius50 <robertomonederoalonso0@gmail.com>
2025-11-19 20:06:26 +0100

    asd

---
+++ src/main/data/Prueba2.sql
@@ -0,0 +1,17 @@
+-- ===========================================
+-- PROYECTO: Prueba2
+-- ENFOQUE: presentacion
+-- FECHA DE CREACIÓN: 2025-11-19T19:46:41.776946500
+-- ===========================================
+
+-- Este archivo es un LOG/HISTORIAL de las operaciones del proyecto.
+-- La base de datos 'viva' es MySQL.
+
+use worldbuilding;
+
+
+-- Operación agregada: 2025-11-19T19:49:59.669173700
+INSERT INTO entidadIndividual (nombre, apellidos, estado, tipo, origen, comportamiento, descripcion) VALUES ('ahda', 'ajshdkjasd', 'activo', 'entidadindividual', 'realista', 'activo', 'preese');
+
+-- Operación agregada: 2025-11-19T19:55:57.441485
+INSERT INTO entidadColectiva (nombre, apellidos, estado, tipo, origen, comportamiento, descripcion) VALUES ('dgfd', 'sfdsf', 'Muerto', 'entidadcolectiva', 'realista', 'pasivo', 'fasfd');
